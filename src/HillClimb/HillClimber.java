00b6852f2a437989c10ca16c098cfed590c7ef0d
Kyle Richardson <richarkc@miamioh.edu>
2015-10-02 15:48:24 -0400

    I made the algorithm retarded and it works 97% of the time thanks obama

--- src/HillClimb/HillClimber.java
+++ src/HillClimb/HillClimber.java
@@ -1,67 +1,50 @@
 package HillClimb;
 
+import java.util.Random;
+
 public class HillClimber {
 
 	Board board;
 	boolean climbing;
 	public static int correct;
-	int[][] lowestCol = new int[8][2];
-	
+	Random r = new Random();
+	long time;
 
 	public HillClimber() {
 		board = new Board();
 		board.calculateSquareCollisions();
-		climbing = true;
-		while (climbing) {
-			climbing = false;
-			
+		time = System.currentTimeMillis();
+		while (board.totalCollisions() != 0) {
 			for (int i = 0; i < 8; i++) {
-
-				int[] ret = climbHelper(i);
-				lowestCol[i][0] = ret[0];
-				lowestCol[i][1] = ret[1];
-				if (ret[2] != Integer.MAX_VALUE) {
-					climbing = true;					
-					switchQueen(i, ret[1], ret[0]);
+				if (climbHelper(i)) {
 					board.calculateSquareCollisions();
-					System.out.println(board);
 				}
-				
 
 			}
 
-			
-			
-			
+			if (System.currentTimeMillis() - time > 100)
+				return;
 
 		}
-		
-		if (board.totalCollisions() == 0)
-			correct++;
-		
+
+		correct++;
+
 	}
 
-	private int[] climbHelper(int column) {
-		int lowest = Integer.MAX_VALUE, occ = -1, lowestNum = -1;
+	private boolean climbHelper(int column) {
+		int occ = -1;
 
 		for (int i = 0; i < 8; i++) {
-			Square temp = board.board[i][column];
-			if (temp.getCollisions() < lowest) {
-				lowest = temp.getCollisions();
-				lowestNum = i;
-			}
-			if (temp.isOccupied()){
+			if (board.board[i][column].isOccupied())
 				occ = i;
-			}
 		}
-
-		if (lowestNum != occ) {
-			int[] ret = { lowestNum, occ, lowest };
-			return ret;
-		} else {
-			int[] ret = { lowestNum, occ, Integer.MAX_VALUE };
-			return ret;
+		int m = r.nextInt(8);
+		if (board.board[m][column].getCollisions() <= board.board[occ][column]
+				.getCollisions()) {
+			switchQueen(column, occ, m);
+			return true;
 		}
+		return false;
 
 	}
 
